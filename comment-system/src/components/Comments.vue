669a0600157fccec8cea939c097510cba24bcc28
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-09-22 15:08:37 -0300

    Create FormComment component

--- comment-system/src/components/Comments.vue
+++ comment-system/src/components/Comments.vue
@@ -2,17 +2,10 @@
     <div class="container">
         <h1>Comments</h1>
         <hr />
-        <div class="form-comment form-froup">
-        <p>
-            <input placeholder="name" type="text" name="author" class="form-control" v-model="name" />
-        </p>
-        <p>
-            <textarea placeholder="comment" name="message" class="form-control" v-model="message" ></textarea>
-        </p>
-        <button v-on:click="addComment" class="btn btn-primary">Submit</button>
-        </div>
+        <FormComment v-on:add-comment="addComment" />
         <hr />
             <div class="list-group">
+                <p>No comments...</p>
                 <div class="list-group-item" v-for="comment, index in allComments">
                 <span class="comment__author">Author: <strong>{{ comment.name }}</strong></span>
                 <p>{{ comment.message }}</p>
@@ -25,26 +18,19 @@
     </div>
 </template>
 <script>
+    import FormComment from './FormComment.vue';
     export default {
+        components: {
+            FormComment
+        },
         data() {
                 return {
                     comments:[],
-                    name: "",
-                    message: ""
                 }
         },
         methods: {
-            addComment() {
-                if(this.message.trim() === '') {
-                    return alert('Please fill in the form!');
-                }
-                this.comments.push({
-                    name: this.name,
-                    message: this.message
-                });
-                
-                this.name = "";
-                this.message = "";
+            addComment(comment) {
+                this.comments.push(comment);
             },
             removeComment(index) {
                 this.comments.splice(index, 1);
---
+++ comment-system/src/components/FormComment.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="form-comment form-froup">
+        <p>
+            <input placeholder="name" type="text" name="author" class="form-control" v-model="name" />
+        </p>
+        <p>
+            <textarea placeholder="comment" name="message" class="form-control" v-model="message" ></textarea>
+        </p>
+        <button v-on:click="addComment" class="btn btn-primary">Submit</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name: "",
+                message: ""
+            }
+        },
+        methods: {
+            addComment() {
+                if(this.message.trim() === '') {
+                    return alert('Please fill in the form!');
+                }
+                
+                this.$emit('add-comment', {
+                    name: this.name,
+                    message: this.message
+                });
+                
+                this.name = "";
+                this.message = "";
+            }
+        }
+
+    }
+</script>
