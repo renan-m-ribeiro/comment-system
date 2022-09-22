2211378b22e87f1d909dba168ef9caf8e94e2544
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-09-22 13:50:38 -0300

    Install Vue.js

---
+++ comment-system/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Comments from './components/Comments.vue';
+
+  export default {
+    name: "App",
+    componets: {
+        Comments
+    },
+    components: { Comments }
+}
+</script>
+
+<template>
+ <Comments />
+</template>
+
+<style>
+
+</style>
---
+++ comment-system/src/components/Comments.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+            <h1>Comments</h1>
+            <hr />
+                <div class="form-comment form-froup">
+                <p>
+                    <input placeholder="name" type="text" name="author" class="form-control" />
+                </p>
+                <p>
+                    <textarea placeholder="comment" name="message" class="form-control" ></textarea>
+                </p>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+                <hr />
+                <div class="list-group">
+                    <div class="list-group-item" v-for="comment in comments">
+                    <span class="comment__author">Author: <strong>{{ comment.name }}</strong></span>
+                    <p>{{ comment.message }}</p>
+                        <div>
+                            <a href="#" title="Delete">Delete</a>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+            </div>
+</template>
+<script>
+    export default {
+        data() {
+                return {
+                    comments:[
+                        {
+                            name: "Ayrton",
+                            message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae."
+                        },
+                    ]
+                }
+        }
+    }
+</script>
